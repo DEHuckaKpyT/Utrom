2463b34f97b8563a87ce4c7cfe37b6dec30ba5bd
DEHuckaKpyT <den-matytsin@mail.ru>
2021-04-09 20:39:36 +1000

    Add ProcessDirectories

---
+++ Utrom/Utrom/LocalDirectoryInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utrom
+{
+    class LocalDirectoryInfo
+    {
+        public string Name { get; }
+        public string UUID { get; }
+        public int Weight { get; }
+        public string FullName { get; }
+
+        public LocalDirectoryInfo(string name, string UUID, int weight, string fullName)
+        {
+            Name = name;
+            this.UUID = UUID;
+            Weight = weight;
+            FullName = fullName;
+        }
+    }
+}
