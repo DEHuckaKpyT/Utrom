07ce4bab01645bf8c038a6be14c3fdb44add1f00
DEHuckaKpyT <den-matytsin@mail.ru>
2021-04-09 18:49:39 +1000

    Add ProcessFiles

--- Utrom/Utrom/Program.cs
+++ Utrom/Utrom/Program.cs
@@ -21,13 +21,39 @@ namespace Utrom
         {
             DirectoryInfo mainDirectory = new DirectoryInfo(path);
             DirectoryInfo[] directories = mainDirectory.GetDirectories();
-            FileInfo[] files = mainDirectory.GetFiles();
-
-
+            ProcessFiles(mainDirectory.GetFiles());
 
         }
 
-        static int GetValue(string path)
+        static void ProcessFiles(FileInfo[] files)
+        {
+            string prevFileName = "";
+            string curFileName = "";
+            string curFileUUID = "";
+            List<LocaFileInfo> locaFiles = new List<LocaFileInfo>();
+            foreach (FileInfo file in files)
+            {
+                curFileName = file.Name.Remove(file.Name.LastIndexOf(' '));
+                curFileUUID = file.Name.Remove(0, file.Name.LastIndexOf(' ')).Remove(file.Name.LastIndexOf('.'));
+                LocaFileInfo curFile = new LocaFileInfo(curFileName, curFileUUID, GetWeight(file.FullName));
+                if (prevFileName == curFileName)
+                {
+                    locaFiles.Add(curFile);
+                }
+                else
+                {
+                    locaFiles.OrderBy(c => c.Weight).ThenBy(c => c.UUID);
+                    //Write(locaFiles);
+                    locaFiles.Clear();
+                    locaFiles.Add(curFile);
+                    prevFileName = curFileName;
+                }
+            }
+            locaFiles.OrderBy(c => c.Weight).ThenBy(c => c.UUID);
+            //Write(locaFiles);
+        }
+
+        static int GetWeight(string path)
         {
             using (StreamReader stream = new StreamReader(path))
                 return int.Parse(stream.ReadLine().Replace("Kb", ""));
