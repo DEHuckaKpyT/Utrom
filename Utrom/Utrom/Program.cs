599b1b25e959876429bff9c4decf38440be70e3e
DEHuckaKpyT <den-matytsin@mail.ru>
2021-04-09 17:49:39 +1000

    Add GetValue

--- Utrom/Utrom/Program.cs
+++ Utrom/Utrom/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,29 @@ namespace Utrom
     {
         static void Main(string[] args)
         {
+
+
+
+            Console.ReadKey();
         }
+
+        static void Get(string path)
+        {
+            DirectoryInfo mainDirectory = new DirectoryInfo(path);
+            DirectoryInfo[] directories = mainDirectory.GetDirectories();
+            FileInfo[] files = mainDirectory.GetFiles();
+
+
+
+        }
+
+        static int GetValue(string path)
+        {
+            using (StreamReader stream = new StreamReader(path))
+                return int.Parse(stream.ReadLine().Replace("Kb", ""));
+        }
+
+
+
     }
 }
