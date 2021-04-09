693038c559f97319af6f287d14facd51728b07da
DEHuckaKpyT <den-matytsin@mail.ru>
2021-04-09 18:11:45 +1000

    Add class LocaFileInfo

---
+++ Utrom/Utrom/LocaFileInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utrom
+{
+    class LocaFileInfo
+    {
+        public string Name { get; }
+        public string UUID { get; }
+        public int Weight { get; }
+
+        public LocaFileInfo(string name, string UUID, int weight)
+        {
+            Name = name;
+            this.UUID = UUID;
+            Weight = weight;
+        }
+    }
+}
